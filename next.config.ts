66fdfc97f12a64500bb0d9557ecc5c64766ceec8
as00000000 <anitsarkar04@gmail.com>
2026-05-04 23:28:50 +0530

    see

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   devIndicators: {
@@ -12,16 +11,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingIncludes: {
-    "/**": [
-      path.join(process.cwd(), "lib/generated/prisma/**/*"),
-      path.join(process.cwd(), "node_modules/.prisma/**/*"),
-      path.join(process.cwd(), "node_modules/@prisma/engines/**/*"),
-      path.join(process.cwd(), "node_modules/@prisma/**/*"),
-    ],
-  },
-  // Ensure Prisma client is available to serverless functions
-  serverExternalPackages: ["@prisma/client"],
+  
 };
 
 export default nextConfig;
